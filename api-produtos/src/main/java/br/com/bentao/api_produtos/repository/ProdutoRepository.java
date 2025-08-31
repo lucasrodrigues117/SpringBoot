cedff3f5f9796737b8b34b0c0861ddf087e85e5d
lucasrodrigues117 <rodrigues.lds.lfr@gmail.com>
2025-08-30 22:20:02 -0300

    Adiciona conteúdo dentro dos arquivos.java e testa a instalação do Maven

--- api-produtos/src/main/java/br/com/bentao/api_produtos/repository/ProdutoRepository.java
+++ api-produtos/src/main/java/br/com/bentao/api_produtos/repository/ProdutoRepository.java
@@ -1,5 +1,12 @@
 package br.com.bentao.api_produtos.repository;
 
-public class ProdutoRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.bentao.api_produtos.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+    List<Produto> findByNome(String nome);
+    List<Produto> findByNomeContaining(String parteDoNome);
 }
